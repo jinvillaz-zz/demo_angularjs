1091ec5d661b134f60f89b8ac1c57aad0e1b4b4d
Jhonatan Villanueva <jinvillaz@gmail.com>
2018-05-27 18:06:29 -0400

    remove tocken for request in mode develop.

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,11 @@ export default routesConfig;
  * Defines route path for having ordered views.
  * @param {Object} $stateProvider angular native code.
  * @param {Object} $urlRouterProvider angular native code.
- * @param {Object} $httpProvider angular native code.
  */
-function routesConfig($stateProvider, $urlRouterProvider, $httpProvider) {
+function routesConfig($stateProvider, $urlRouterProvider) {
     'ngInject';
     $urlRouterProvider.otherwise('/');
-    $httpProvider.defaults.headers.common.Authorization = 'bearer ' +
-        'a6be589b3316edb2855589ab1e7d35c4e16dab68';
+
     $stateProvider
     .state('main', {
         url: '/',
